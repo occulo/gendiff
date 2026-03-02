60774abc3b82c2e4ee67e1d1a227a2ce5a040913
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-03-02 22:23:51 +0400

    refactor(genDiff): move main logic from executable to helper function

---
+++ src/genDiff.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Differ;
+
+use Hexlet\Code\Differ as DiffService;
+use Hexlet\Code\ParserFactory;
+use Hexlet\Code\FormatterFactory;
+
+function genDiff(string $firstPath, string $secondPath, string $format = 'stylish'): string
+{
+    $firstFile = ParserFactory::build($firstPath)->parse($firstPath);
+    $secondFile = ParserFactory::build($secondPath)->parse($secondPath);
+    $diff = (new DiffService())->genDiff($firstFile, $secondFile);
+    return FormatterFactory::build($format)->format($diff);
+}
