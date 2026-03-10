3df33ce9eb905158a67f0779c3d61bd56ae4efb3
Slava Panfilenko <slava.panfilenko@outlook.com>
2026-03-10 19:19:42 +0400

    tests(differ): add test for default behavior

--- tests/DifferTest.php
+++ tests/DifferTest.php
@@ -5,7 +5,7 @@ namespace Tests;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\TestCase;
 
-use function Differ\Differ\genDiff;
+use Differ\Differ\GenDiff;
 
 class DifferTest extends TestCase
 {
@@ -28,12 +28,20 @@ class DifferTest extends TestCase
         'json' => self::FIXTURES_DIR . '/expected_json.txt',
     ];
 
+    public static function fileProvider(): array
+    {
+        $data = [];
+        foreach (self::FILES as $extension => [$firstFile, $secondFile]) {
+            $data[$extension] = [$firstFile, $secondFile];
+        }
+        return $data;
+    }
+
     public static function fileAndFormatProvider(): array
     {
         $formats = array_keys(self::EXPECTED_OUTPUT);
         $data = [];
         foreach (self::FILES as $extension => [$firstFile, $secondFile]) {
-            $data["{$extension}: default"] = [$firstFile, $secondFile];
             foreach ($formats as $format) {
                 $data["{$extension}: {$format}"] = [$firstFile, $secondFile, $format];
             }
@@ -41,10 +49,20 @@ class DifferTest extends TestCase
         return $data;
     }
 
-    #[DataProvider('fileAndFormatProvider')]
-    public function testGenDiff(string $firstPath, string $secondPath, string $format = 'stylish'): void
+    #[DataProvider('fileProvider')]
+    public function testGenDiff(string $firstPath, string $secondPath): void
     {
-        $actual = genDiff($firstPath, $secondPath, $format);
+        $gendiff = new GenDiff();
+        $actual = $gendiff->genDiff($firstPath, $secondPath);
+        $expected = self::EXPECTED_OUTPUT['stylish'];
+        $this->assertStringEqualsFile($expected, $actual);
+    }
+
+    #[DataProvider('fileAndFormatProvider')]
+    public function testGenDiffWithFormat(string $firstPath, string $secondPath, string $format): void
+    {
+        $gendiff = new GenDiff();
+        $actual = $gendiff->genDiff($firstPath, $secondPath, $format);
         $expected = self::EXPECTED_OUTPUT[$format];
         $this->assertStringEqualsFile($expected, $actual);
     }
